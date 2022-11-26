6bc258943ac123be5f0f9cdfc8201a67f12826de
KushagraAgarwal525 <kushagraagarwal525.ka@gmail.com>
2022-11-27 00:16:52 +0530

    Add code for discover page

---
+++ pages/discover.tsx
@@ -0,0 +1,114 @@
+export default function Discover() {
+  return (
+    <>
+        <div className='top-navbar'></div>
+        <div className="discover-page">
+            <div className="discover-sections-p">
+                <div className='discover-sections'>
+                    <div className='discover-section'>
+                        Discover
+                    </div>
+                    <div className='discover-section'>
+                        Tech
+                    </div>
+                    <div className='discover-section'>
+                        Physical
+                    </div>
+                    <div className='discover-section'>
+                        Teaching
+                    </div>
+                    <div className='discover-section'>
+                        Design
+                    </div>
+                    <div className='discover-section'>
+                        Development
+                    </div>
+                    <div className='discover-section'>
+                        Online
+                    </div>
+                </div>
+            </div>
+            <div className="listings">
+                <div className="listing">
+                    <div className="listing-title">E-Commerce Website</div>
+                    <div className="listing-tags">
+                        <div className="listing-tag">Design</div>
+                        <div className="listing-tag">Web</div>
+                        <div className="listing-tag">Development</div>
+                        <div className="listing-tag">Backend</div>
+                        <div className="listing-tag">E-Commerce</div>
+                    </div>
+                    <div className="listing-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam odio est, porttitor sit amet urna sit amet, malesuada dictum felis. Sed arcu leo, tristique sit amet interdum sit amet, auctor vel mi.</div>
+                    <div className="listing-location"><span></span></div>
+                    <div className="listing-author"><span></span></div>
+                </div>
+                <div className="listing">
+                    <div className="listing-title">E-Commerce Website</div>
+                    <div className="listing-tags">
+                        <div className="listing-tag">Design</div>
+                        <div className="listing-tag">Web</div>
+                        <div className="listing-tag">Development</div>
+                        <div className="listing-tag">Backend</div>
+                        <div className="listing-tag">E-Commerce</div>
+                    </div>
+                    <div className="listing-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam odio est, porttitor sit amet urna sit amet, malesuada dictum felis. Sed arcu leo, tristique sit amet interdum sit amet, auctor vel mi.</div>
+                    <div className="listing-location"><span></span></div>
+                    <div className="listing-author"><span></span></div>
+                </div>
+                <div className="listing">
+                    <div className="listing-title">E-Commerce Website</div>
+                    <div className="listing-tags">
+                        <div className="listing-tag">Design</div>
+                        <div className="listing-tag">Web</div>
+                        <div className="listing-tag">Development</div>
+                        <div className="listing-tag">Backend</div>
+                        <div className="listing-tag">E-Commerce</div>
+                    </div>
+                    <div className="listing-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam odio est, porttitor sit amet urna sit amet, malesuada dictum felis. Sed arcu leo, tristique sit amet interdum sit amet, auctor vel mi.</div>
+                    <div className="listing-location"><span></span></div>
+                    <div className="listing-author"><span></span></div>
+                </div>
+                <div className="listing">
+                    <div className="listing-title">E-Commerce Website</div>
+                    <div className="listing-tags">
+                        <div className="listing-tag">Design</div>
+                        <div className="listing-tag">Web</div>
+                        <div className="listing-tag">Development</div>
+                        <div className="listing-tag">Backend</div>
+                        <div className="listing-tag">E-Commerce</div>
+                    </div>
+                    <div className="listing-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam odio est, porttitor sit amet urna sit amet, malesuada dictum felis. Sed arcu leo, tristique sit amet interdum sit amet, auctor vel mi.</div>
+                    <div className="listing-location"><span></span></div>
+                    <div className="listing-author"><span></span></div>
+                </div>
+                <div className="listing">
+                    <div className="listing-title">E-Commerce Website</div>
+                    <div className="listing-tags">
+                        <div className="listing-tag">Design</div>
+                        <div className="listing-tag">Web</div>
+                        <div className="listing-tag">Development</div>
+                        <div className="listing-tag">Backend</div>
+                        <div className="listing-tag">E-Commerce</div>
+                    </div>
+                    <div className="listing-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam odio est, porttitor sit amet urna sit amet, malesuada dictum felis. Sed arcu leo, tristique sit amet interdum sit amet, auctor vel mi.</div>
+                    <div className="listing-location"><span></span></div>
+                    <div className="listing-author"><span></span></div>
+                </div>
+                <div className="listing">
+                    <div className="listing-title">E-Commerce Website</div>
+                    <div className="listing-tags">
+                        <div className="listing-tag">Design</div>
+                        <div className="listing-tag">Web</div>
+                        <div className="listing-tag">Development</div>
+                        <div className="listing-tag">Backend</div>
+                        <div className="listing-tag">E-Commerce</div>
+                    </div>
+                    <div className="listing-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam odio est, porttitor sit amet urna sit amet, malesuada dictum felis. Sed arcu leo, tristique sit amet interdum sit amet, auctor vel mi.</div>
+                    <div className="listing-location"><span></span></div>
+                    <div className="listing-author"><span></span></div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
