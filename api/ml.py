47683250f655fe035531f71302a95864a0a68128
Kevin Hwang <timmyhwang127@gmail.com>
2022-11-26 11:46:56 -0500

    readded ml

--- api/ml.py
+++ api/ml.py
@@ -1,17 +1,14 @@
-# from api.extensions import skillExtractor
-#
-# def skill_get(desc):
-#     skill_ls = []
-#     annotations = skillExtractor.annotate(desc)["results"]
-#     fm = annotations["full_matches"][0]
-#     ng = annotations["ngram_scored"]
-#
-#     skill_ls.append(fm["doc_node_value"])
-#
-#     for i in ng:
-#         skill_ls.append(i["doc_node_value"])
-#
-#     return skill_ls
+from api.extensions import skillExtractor
 
 def skill_get(desc):
-    return ["node"]
+    skill_ls = []
+    annotations = skillExtractor.annotate(desc)["results"]
+    fm = annotations["full_matches"][0]
+    ng = annotations["ngram_scored"]
+
+    skill_ls.append(fm["doc_node_value"])
+
+    for i in ng:
+        skill_ls.append(i["doc_node_value"])
+
+    return skill_ls
