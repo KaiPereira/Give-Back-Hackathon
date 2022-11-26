7a3c6c31a86d6e796df435c6718902c443860498
Kevin Hwang <timmyhwang127@gmail.com>
2022-11-26 13:09:16 -0500

    chja

--- api/ml.py
+++ api/ml.py
@@ -3,7 +3,11 @@ from api.extensions import skillExtractor
 def skill_get(desc):
     skill_ls = []
     annotations = skillExtractor.annotate(desc)["results"]
-    fm = annotations["full_matches"][0]
+
+    try:
+        fm = annotations["full_matches"][0]
+    except IndexError:
+        return ["job experience"]
     ng = annotations["ngram_scored"]
 
     skill_ls.append(fm["doc_node_value"])
