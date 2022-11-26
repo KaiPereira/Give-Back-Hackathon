3b9e909a419e1bdb089fa9217525e684c5656897
KaiPereira <88850028+KaiPereira@users.noreply.github.com>
2022-11-25 17:09:28 -0800

    Created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Give Back Hackathon</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
